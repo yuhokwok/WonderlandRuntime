efe414adbbdf2ebeb472b25a53e95ca64df02ff8
Will Kwok <yuho1985@gmail.com>
2024-10-29 22:36:19 +0800

    first try

--- Package.swift
+++ Package.swift
@@ -5,17 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "WonderlandRuntime",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WonderlandRuntime",
             targets: ["WonderlandRuntime"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/maxxfrazer/FocusEntity.git", from: "v1.0.0"),
+        .package(url: "https://github.com/maxxfrazer/RealityGeometries.git", from: "v1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WonderlandRuntime"),
+            name: "WonderlandRuntime",
+            exclude: ["README.md"]
+        ),
         .testTarget(
             name: "WonderlandRuntimeTests",
             dependencies: ["WonderlandRuntime"]
--- Sources/WonderlandRuntime/WonderlandRuntime.swift
+++ Sources/WonderlandRuntime/WonderlandRuntime.swift
@@ -1,2 +1,15 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import SwiftUI
+import RealityKit
+import ARKit
+
+struct WonderlandRuntimeView : View {
+    var body : some View {
+        VStack  {
+            Text("Hello Wonderland")
+        }
+    }
+}
